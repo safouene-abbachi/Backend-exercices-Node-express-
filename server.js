5feae8d1a13ff22dab791eb3f8a5c93c32af3ed8
safouene <safouene-abbachi@hotmail.fr>
2022-08-07 20:47:29 +0100

    DEV : Deploy with heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(cors());
-
+app.use(express.static('build'));
 app.use(express.json());
 
 const data = [
